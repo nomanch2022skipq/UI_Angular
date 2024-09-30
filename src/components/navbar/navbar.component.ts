92c9abf5920691e5fb5dbbbc9854275df69a5367
nomanch2022skipq <nomanch2022skipq@gmail.com>
2024-09-30 21:57:09 +0500

    issue #3 creare logina and register page

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCoffee, faMarsAndVenus, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { SharedService } from '../../services/shared.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [MatIconModule, FontAwesomeModule, CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-
-  constructor(public sharedService: SharedService){}
-
+export class NavbarComponent implements OnInit {
+  
   faBars = faBars;
+  isAuthenticated: boolean = false;
+
+  constructor(public sharedService: SharedService, public apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.authStatus$.subscribe(isAuth => {
+      this.isAuthenticated = isAuth;
+      console.log(this.isAuthenticated); // Debugging
+    });
+  }
+
+  logout() {
+    this.apiService.logout();
+  }
 }
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,13 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  constructor(public http: HttpClient) {}
+  apiUrl = environment.API_URL;
+
+  private authenticatedSubject = new BehaviorSubject<boolean>(
+    this.isAuthenticated()
+  );
+  authStatus$ = this.authenticatedSubject.asObservable();
+
+  constructor(public http: HttpClient, private router: Router) {}
+
+  login(data: any): Observable<any> {
+    return this.http.post(this.apiUrl + 'login/', data).pipe(
+      tap((response: any) => {
+        this.authenticatedSubject.next(true);
+      })
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.authenticatedSubject.next(false); // Update authentication status
+    this.router.navigate(['/login']); // Redirect to login page
+  }
+
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('token');
+  }
 
   addContact(data: any) {
-    return this.http.post('http://127.0.0.1:8000/api/contacts/', data);
+    return this.http.post(this.apiUrl + 'contacts/', data);
   }
 }
